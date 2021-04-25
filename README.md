2e2fa0dcba8c9bde5efa9955b6327ab45ac75c16
Wil Thieme <wil.thieme@protonmail.com>
2021-04-25 18:14:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# FG-PFRPG-Encumbrance Penalties
- This extension enhances functionality related to carrying capacity and weight/armor encumbrance.
+# PFRPG Encumbrance Penalties
+This extension enhances functionality related to carrying capacity and weight/armor encumbrance.
+
+# Features
+* Automate encumbrance penalties based on total weight and current carrying capacity. The penalties are now colored to indicate their source. Red is for encumbrance based on weight and black is for armor.
+* Update carrying capacities when strength-modifying effects are applied/changed/removed. Currently supported: 'STR: N'(increases strength score and carrying capacity ONLY IN 3.5E), 'CARRY: N'(increases strength score only for calculating carrying capacity), and 'Ant Haul'(triples carrying capacity).
+* Auto-change speed based on weight encumbrance and some supported Pathfinder conditions.
+
+# Compatibility and Instructions
+This extension has been tested with [FantasyGrounds Unity](https://www.fantasygrounds.com/home/FantasyGroundsUnity.php) 4.0.10 (2020-02-04).
---
+++ campaign/scripts/char_inv_TE.lua
@@ -0,0 +1,198 @@
+-- 
+--	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+---	Locate the effects node within the relevant player character's node within combattracker
+--	@param node the databasenode passed along when this file is initialized
+--	@return nodeCharCT path to this PC's databasenode "effects" in the combat tracker
+local function getNodeCharCT(node)
+	local rActor
+	local nodeCharCT
+	if node.getParent().getName() == 'charsheet' then
+		rActor = ActorManager.resolveActor(node)
+	elseif node.getChild('...').getName() == 'charsheet' then
+		rActor = ActorManager.resolveActor(node.getParent())
+	elseif node.getChild('....').getName() == 'charsheet' then -- this might not be necessary
+		rActor = ActorManager.resolveActor(node.getChild('...'))
+	end
+		nodeCharCT = ActorManager.getCTNode(rActor)
+
+	return nodeCharCT
+end
+
+function onInit()
+	local node = getDatabaseNode()
+
+	onEncumbranceChanged() -- this is now handled by char_invlist_TE
+
+	local nodeCharCT = getNodeCharCT(node)
+
+	DB.addHandler(DB.getPath(nodeCharCT, 'effects.*.label'), 'onUpdate', onEffectChanged)
+	DB.addHandler(DB.getPath(nodeCharCT, 'effects.*.isactive'), 'onUpdate', onEffectChanged)
+	DB.addHandler(DB.getPath(nodeCharCT, 'effects'), 'onChildDeleted', onEffectRemoved)
+	DB.addHandler(DB.getPath(node, 'abilities.strength'), 'onChildUpdate', onStrengthChanged)
+	DB.addHandler(DB.getPath(node, 'size'), 'onUpdate', onSizeChanged)
+	DB.addHandler(DB.getPath(node, 'encumbrance.stradj'), 'onUpdate', onStrengthChanged)
+	DB.addHandler(DB.getPath(node, 'encumbrance.carrymult'), 'onUpdate', onEncumbranceChanged)
+	DB.addHandler(DB.getPath('options.ENCUMBRANCE_UNIT'), 'onUpdate', onEncumbranceChanged)
+end
+
+function onClose()
+	local node = getDatabaseNode()
+	local nodeCharCT = getNodeCharCT(node)
+
+	DB.removeHandler(DB.getPath(nodeCharCT, 'effects.*.label'), 'onUpdate', onEffectChanged)
+	DB.removeHandler(DB.getPath(nodeCharCT, 'effects.*.isactive'), 'onUpdate', onEffectChanged)
+	DB.removeHandler(DB.getPath(nodeCharCT, 'effects'), 'onChildDeleted', onEffectRemoved)
+	DB.removeHandler(DB.getPath(node, 'abilities.strength'), 'onChildUpdate', onStrengthChanged)
+	DB.removeHandler(DB.getPath(node, 'size'), 'onUpdate', onSizeChanged)
+	DB.removeHandler(DB.getPath(node, 'encumbrance.stradj'), 'onUpdate', onStrengthChanged)
+	DB.removeHandler(DB.getPath(node, 'encumbrance.carrymult'), 'onUpdate', onEncumbranceChanged)
+	DB.removeHandler(DB.getPath('options.ENCUMBRANCE_UNIT'), 'onUpdate', onEncumbranceChanged)
+end
+
+function onEffectChanged(node)
+	onStrengthChanged()
+end
+
+function onEffectRemoved(node)
+	onStrengthChanged()
+end
+
+---	Determine the total bonus to carrying capacity from effects STR or CARRY
+--	@param rActor a table containing relevant paths and information on this PC
+--	@return nStrEffectMod the PC's current strength score after all bonuses are applied
+local function getStrEffectBonus(rActor)
+	if not rActor then
+		return 0
+	end
+
+	local nStrEffectMod = 0
+	
+	if EffectManager35EDS.hasEffectCondition(rActor, 'Exhausted') then
+		nStrEffectMod = nStrEffectMod - 6
+	elseif EffectManager35EDS.hasEffectCondition(rActor, 'Fatigued') then
+		nStrEffectMod = nStrEffectMod - 2
+	end
+
+	if EffectManager35EDS.hasEffectCondition(rActor, 'Paralyzed') then
+		nStrEffectMod = -1 * nStrEffectMod
+	end
+
+	local nStrEffects = EffectManager35EDS.getEffectsBonus(rActor, 'STR', true)
+--	include STR effects in calculating carrying capacity (only if CARRY_CAPACITY_FROM_EFFECTS is enabled in options)
+--	if OptionsManager.isOption('CARRY_CAPACITY_FROM_EFFECTS', 'on') then
+	if nStrEffects and not DataCommon.isPFRPG() then nStrEffectMod = nStrEffectMod + nStrEffects end
+--	end
+
+	local nCarryBonus = EffectManager35EDS.getEffectsBonus(rActor, 'CARRY', true)
+	if nCarryBonus then
+		nStrEffectMod = nStrEffectMod + nCarryBonus
+	end
+
+	return nStrEffectMod
+end
+
+function onStrengthChanged()
+	onEncumbranceChanged()
+end
+
+function onSizeChanged()
+	onEncumbranceChanged()
+end
+
+function onEncumbranceChanged()
+	local rActor, nodeChar
+
+	if getDatabaseNode().getParent().getName() == 'charsheet' then
+		nodeChar = getDatabaseNode()
+		rActor = ActorManager.resolveActor(nodeChar)
+	elseif getDatabaseNode().getName() == 'effects' then
+		rActor = ActorManager.resolveActor(getDatabaseNode())
+		if ActorManager.isPC(rActor) then
+			nodeChar = ActorManager.getCreatureNode(rActor)
+		end
+	--else
+	--	return
+	end
+
+	local nHeavy = 0
+	local nStrength = DB.getValue(nodeChar, 'abilities.strength.score', 10)
+	local nStrengthDamage = DB.getValue(nodeChar, 'abilities.strength.damage', 0)
+	if DataCommon.isPFRPG() then nStrengthDamage = 0 end
+
+	if DB.getValue(nodeChar, 'encumbrance.stradj', 0) == 0 and CharManager.hasTrait(nodeChar, 'Muscle of the Society') then
+		DB.setValue(nodeChar, 'encumbrance.stradj', 2)
+	end
+
+	local nStrengthAdj = DB.getValue(nodeChar, 'encumbrance.stradj', 0)
+
+	nStrength = nStrength + nStrengthAdj
+	
+	local nStrEffectMod = getStrEffectBonus(rActor)
+	DB.setValue(nodeChar, 'encumbrance.strbonusfromeffects', 'number', nStrEffectMod)
+
+	nStrength = nStrength + nStrEffectMod - nStrengthDamage
+	
+	if nStrength > 0 then
+		if nStrength <= 10 then
+			nHeavy = nStrength * 10
+		else
+			nHeavy = 1.25 * math.pow(2, math.floor(nStrength / 5)) * math.floor((20 * math.pow(2, math.fmod(nStrength, 5) / 5)) + 0.5)
+		end
+	end
+	
+	nHeavy = math.floor(nHeavy * DB.getValue(nodeChar, 'encumbrance.carrymult', 1))
+
+	-- Check for carrying capacity multiplier attached to PC on combat tracker. If found, multiply their carrying capacity.
+	local nCarryMult = EffectManager35EDS.getEffectsBonus(rActor, 'CARRYMULT', true) or 0
+	if nCarryMult ~= 0 then
+		nHeavy = nHeavy * nCarryMult
+	end
+
+	local nLight = math.floor(nHeavy / 3)
+	local nMedium = math.floor((nHeavy / 3) * 2)
+	local nLiftOver = nHeavy
+	local nLiftOff = nHeavy * 2
+	local nPushDrag = nHeavy * 5
+
+	local nSize = ActorManager35E.getSize(rActor)
+	if (nSize < 0) then
+		local nMult = 0
+		if (nSize == -1) then
+			nMult = 0.75
+		elseif (nSize == -2) then
+			nMult = 0.5
+		elseif (nSize == -3) then
+			nMult = .25
+		elseif (nSize == -4) then
+			nMult = .125
+		end
+
+		nLight = math.floor(((nLight * nMult) * 100) + 0.5) / 100
+		nMedium = math.floor(((nMedium * nMult) * 100) + 0.5) / 100
+		nHeavy = math.floor(((nHeavy * nMult) * 100) + 0.5) / 100
+		nLiftOver = math.floor(((nLiftOver * nMult) * 100) + 0.5) / 100
+		nLiftOff = math.floor(((nLiftOff * nMult) * 100) + 0.5) / 100
+		nPushDrag = math.floor(((nPushDrag * nMult) * 100) + 0.5) / 100
+	elseif (nSize > 0) then
+		local nMult = math.pow(2, nSize)
+
+		nLight = nLight * nMult
+		nMedium = nMedium * nMult
+		nHeavy = nHeavy * nMult
+		nLiftOver = nLiftOver * nMult
+		nLiftOff = nLiftOff * nMult
+		nPushDrag = nPushDrag * nMult
+	end
+
+	DB.setValue(nodeChar, 'encumbrance.lightload', 'number', nLight)
+	DB.setValue(nodeChar, 'encumbrance.mediumload', 'number', nMedium)
+	DB.setValue(nodeChar, 'encumbrance.heavyload', 'number', nHeavy)
+	DB.setValue(nodeChar, 'encumbrance.liftoverhead', 'number', nLiftOver)
+	DB.setValue(nodeChar, 'encumbrance.liftoffground', 'number', nLiftOff)
+	DB.setValue(nodeChar, 'encumbrance.pushordrag', 'number', nPushDrag)
+	
+	-- This helps with automatically changing the weight units when the option is toggled
+	CharManager.updateEncumbrance(nodeChar)
+end
