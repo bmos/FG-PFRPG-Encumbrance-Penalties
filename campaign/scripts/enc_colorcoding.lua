3e9e9514e9928a060d7a7833d7b2da008fca18fe
Wil Thieme <wil.thieme@protonmail.com>
2022-03-18 15:03:21 -0400

    move colorcoding to lua file

---
+++ campaign/scripts/enc_colorcoding.lua
@@ -0,0 +1,22 @@
+--
+--	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
+--
+
+function onValueChanged()
+  local sArmorStat = getName():gsub('armor', '')
+  local nMaxStat = DB.getValue(getDatabaseNode().getParent(), 'armor' .. sArmorStat)
+  local nMaxStatFromEnc = DB.getValue(getDatabaseNode().getParent(), sArmorStat .. 'fromenc')
+  if window.usearmormaxstatbonus.getValue() == 0 then
+    window.armormaxstatbonus.setColor(ColorManager.COLOR_FULL)
+  else
+    if nMaxStatFromEnc == nMaxStat then
+      window.armormaxstatbonus.setColor(ColorManager.COLOR_HEALTH_CRIT_WOUNDS)
+    else
+      window.armormaxstatbonus.setColor(ColorManager.COLOR_FULL)
+    end
+  end
+end
+
+function onInit()
+  onValueChanged()
+end
