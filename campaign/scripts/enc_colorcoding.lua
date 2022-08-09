2da78a29a8fc6b70b19f19e76f2499395b14d8f3
Wil Thieme <wil.thieme@protonmail.com>
2022-08-09 18:14:58 -0400

    fix penalty coloration

--- campaign/scripts/enc_colorcoding.lua
+++ campaign/scripts/enc_colorcoding.lua
@@ -1,19 +1,19 @@
 --
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
--- luacheck: globals onValueChanged getName encumbranceColors
+-- luacheck: globals onValueChanged getName encumbranceColors getValue setColor
 
 function encumbranceColors()
 	local sArmorStat = getName():gsub('armor', '')
 	local nMaxStat = DB.getValue(getDatabaseNode().getParent(), 'armor' .. sArmorStat)
 	local nMaxStatFromEnc = DB.getValue(getDatabaseNode().getParent(), sArmorStat .. 'fromenc')
 	if window.usearmormaxstatbonus.getValue() == 0 then
-		window.armormaxstatbonus.setColor(ColorManager.COLOR_FULL)
+		setColor(ColorManager.COLOR_FULL)
 	else
 		if nMaxStatFromEnc == nMaxStat then
-			window.armormaxstatbonus.setColor(ColorManager.COLOR_HEALTH_CRIT_WOUNDS)
+			setColor(ColorManager.COLOR_HEALTH_CRIT_WOUNDS)
 		else
-			window.armormaxstatbonus.setColor(ColorManager.COLOR_FULL)
+			setColor(ColorManager.COLOR_FULL)
 		end
 	end
 end
